8d6f9e57a4bc71c6edca6ed1791a01365797ecb1
Abhishek Kumar Gupta <abhishekguptaatweb17@gmail.com>
2020-08-19 18:02:29 +0530

    merge intervals

--- ! Leetcode/merge-intervals.cpp
+++ ! Leetcode/merge-intervals.cpp
@@ -16,24 +16,16 @@ public:
         vector<vector<int>> ans;
         
         sort(intervals.begin(),intervals.end(),sortbyFirst);
-        vector<int> cur;
-        cur=intervals[0];
+        ans.push_back(intervals[0]);
         
         for(int i=1;i<intervals.size();i++)
         {
-            if(cur[1]>=intervals[i][0])
-            cur[1]=max(cur[1],intervals[i][1]);
+            if(ans.back()[1]<intervals[i][0])
+            ans.push_back(intervals[i]);
+            
             else
-            {
-                ans.push_back(cur);
-                cur=intervals[i];
-            }
+            ans.back()[1]=max(ans.back()[1],intervals[i][1]);
         }
-        
-        if(ans.empty())
-        ans.push_back(cur);
-        else if(cur!=ans[ans.size()-1])
-        ans.push_back(cur);
         
         return ans;
     }
