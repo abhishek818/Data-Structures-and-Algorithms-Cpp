611bec4c5ed8e04e08664d8e128c3d3b0ebc71be
Abhishek Kumar Gupta <abhishekguptaatweb17@gmail.com>
2020-08-23 23:00:28 +0530

    best time to buy and sell stock-iii

---
+++ ! Leetcode/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,33 @@
+Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+username: abhishek818
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        if(n==0)
+        return 0;
+        
+        vector<int> left(n), right(n);
+        
+        int leftMin=prices[0];
+        for(int i=1;i<n;i++)
+        {
+            left[i]=max(left[i-1], prices[i]-leftMin);
+            leftMin=min(leftMin, prices[i]);
+        }
+        
+        int rightMax=prices[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=max(right[i+1], rightMax-prices[i]);
+            rightMax=max(rightMax, prices[i]);
+        }
+        
+        int profit=right[0];
+        for(int i=1;i<n;i++)
+            profit=max(profit,left[i-1]+right[i]);
+        
+        return profit;
+    }
+};
